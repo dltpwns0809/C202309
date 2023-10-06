d3b302d63d0621136929172203010de94bd4e322
이세준 <k43360154@gmail.com>
2023-10-06 14:43:14 +0900

    int main으로 수정

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -2,10 +2,11 @@
 
 int multiF(int value);// 함수 원형 선언
 
-void main() { 
+int main() { 
 	printf("1부터    2까지의 곱은 %d\n", multiF(2)); //%d의 위치에 multiF()의 함수에서 매개변수 따른 반환값을 입력해 출력
 	printf("1부터    3까지의 곱은 %d\n", multiF(3));//%d의 위치에 multiF()의 함수에서 매개변수 따른 반환값을 입력해 출력
 	printf("1부터    5까지의 곱은 %d\n", multiF(5));//%d의 위치에 multiF()의 함수에서 매개변수 따른 반환값을 입력해 출력
+	return 0;
 }
 
 int multiF(int value) {  // 반환값이 정수형이므로 int 형으로 선언
--- practice/week5/functionHap.c
+++ practice/week5/functionHap.c
@@ -2,10 +2,11 @@
 
 int hapf(int value);   // 함수의 원형을 선언
 
-void main() {
+int main() {
 	printf("1부터      10까지의 합은 %d\n", hapf(10)); // %d의 위치에 hapf()의 함수에서 매개변수 따른 반환값을 입력해 출력
 	printf("1부터    100까지의 합은 %d\n", hapf(100));// %d의 위치에 hapf()의 함수에서 매개변수 따른 반환값을 입력해 출력
 	printf("1부터  1000까지의 합은 %d\n", hapf(1000));// %d의 위치에 hapf()의 함수에서 매개변수 따른 반환값을 입력해 출력
+	return 0;
 }
 
 int hapf(int value) {//반환값이 정수형이므로 int형으로 함수 선언
